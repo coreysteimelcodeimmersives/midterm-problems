f786397430e3d98e44fc61a853a0c19c2d2c8de9
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-03-07 14:12:54 -0700

    day 1 work

--- main.js
+++ main.js
@@ -1,43 +1,172 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2){
+  if (num1 % num2 === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return (Math.pow(num, 2))/2;
 }
 
-function isLong() {
+function isLong(str) {
+  if (str.length >= 15){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function exclaim() {
+// this works too.
+// function exclaim(str){
+//   let newStr = str.replaceAll(`!`,``)
+//   return newStr += `!`
+// }
+
+function exclaim(str) {
+  let newStr = str;
+  for (let i = 0; i < str.length; i++){
+    if (str[str.length-(i+1)] === `!`){
+      newStr = str.slice(0, str.length-(i+1));
+    } else {
+      break;
+    }
+  }
+  return newStr += `!`
 }
 
-function countWords() {
+function countWords(str) {
+  return str.split(` `).length;
 }
 
-function containsDigit() {
+function containsDigit(str) {
+  let digits =`1234567890`;
+  for (let i = 0; i < str.length; i++){
+    if (digits.includes(str[i])){
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+  let lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < str.length; i++){
+    if (lowerCaseChars.includes(str[i])){
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsUpperCase() {
+function containsUpperCase(str) {
+  let upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < str.length; i++){
+    if (upperCaseChars.includes(str[i])){
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+  let alphaNumericChars = `abcdefghijklmnopqrstuvwxyz1234567890`;
+  for (let i = 0; i < str.length; i++){
+    if (!alphaNumericChars.includes(str[i].toLowerCase())){
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  let spaceChar = ` `;
+  for (let i = 0; i < str.length; i++){
+    if (spaceChar.includes(str[i])){
+      return true;
+    }
+  }
+  return false;
 }
 
-function digits() {
+function digits(num) {
+  let strNum = num.toString();
+  let numChars = `12345678790`;
+  let numArray = [];
+  for (let i = 0; i < strNum.length; i++){
+    if (numChars.includes(strNum[i])){
+      numArray.push(Number(strNum[i]));
+    }
+  }
+  return numArray;
 }
 
-function truncate() {
+function truncate(str) {
+  if (str.length < 15){
+    return str;
+  } else {
+    let newStr = ``
+    return newStr = `${str.slice(0,8)}...`
+  }
 }
 
-function isValidPassword() {
+function isValidPassword(str) {
+  let alphaNumericChars = `abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+  let lowerCaseChars = `abcdefghijklmnopqrstuvwxyz`;
+  let upperCaseChars = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+  let numChars = `12345678790`;
+  let space = ` `;
+  let boolNumbers = false;
+  let boolNoSpace = false;
+  let boolContainsUpperCase = false;
+  let boolContainsLowerCase = false;
+  let boolContainsNonAlphaNumeric = false;
+
+  for (let i = 0; i < str.length; i++){
+    if (space.includes(str[i])){
+      return false;
+    } else {
+      boolNoSpace = true;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++){
+    if (lowerCaseChars.includes(str[i])){
+      boolContainsLowerCase = true;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++){
+    if (upperCaseChars.includes(str[i])){
+      boolContainsUpperCase = true;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++){
+    if (!alphaNumericChars.includes(str[i])){
+      boolContainsNonAlphaNumeric = true;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++){
+    if (numChars.includes(str[i])){
+      boolNumbers = true;
+    }
+  }
+
+  if (boolContainsLowerCase === true && boolContainsNonAlphaNumeric === true && boolContainsUpperCase && boolNoSpace == true && boolNumbers === true){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function onlyPunchy() {
+function onlyPunchy(arr) {
+  for (let i = 0; i < arr.length; i++){
+
+  }
+
 }
 
 
